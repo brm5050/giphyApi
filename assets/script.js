6be6f60da0c1d4a299d07198638d36da69f87160
bryan.modula <bryan.modula@gmail.com>
2017-05-03 17:52:17 -0400

    added for loop to run API through each button

--- assets/script.js
+++ assets/script.js
@@ -13,24 +13,43 @@ function displayIngredient(){
       url: queryURL,
       method: 'GET'
     }).done(function(response) {
-      console.log(response);
+      	console.log(response);
+      	var results = response.data;
+     	 for (var i = 0; i < results.length; i++) {
+      	var ingredDiv = $('<div class="ingredientClass">');
+      	var p = $("<p>").text("Rating: " + results[i].rating);
+      	var ingredientImage = $("<img>");
+      	ingredientImage.attr("src", results[i].images.fixed_height.url);
 
-      var ingredDiv = $('<div class="ingredientClass">');
-      //add rating and other data
+      	ingredDiv.append(p);
+        ingredDiv.append(ingredientImage);
+		
+		$("#displayGifs").prepend(ingredDiv);
+  		}
 
-      
       });
 }
 
+
+//make function to create the buttons of the original array ingredients
 function renderButtons(){
+	//making where the buttons get dumped not have duplicates
+	//starting with an empty div
 	$('#ingredientView').empty();
 
+	//dynamic for loop to go through globa var array and get the button info
 	for (var i = 0; i < ingredients.length; i++) {
+		//adding button to html
 		var newButton = $('<button>');
+		//adding class in html to button
 		newButton.addClass('ingredientClass');
+		//adding data-name attribute to each button
 		newButton.attr('data-name' , ingredients[i]);
+		//dynamic functionality that each button's type will be displayed as text
 		newButton.text(ingredients[i]);
+		//taking the newButton var we just made and putting it into the html div
 		$('#ingredientView').append(newButton);
+		//making sure it works
 		console.log(newButton);
 	}
 
